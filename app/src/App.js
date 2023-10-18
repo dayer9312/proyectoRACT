56667ba03b5857f8dd268fcc67c4467141854a25
dayer9312 <dayer.9312@gmail.com>
2023-10-18 09:28:30 -0400

    primer componente creado

---
+++ app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hola mundo
+        </p>
+        <a
+          className="App-link"
+          href="https://www.google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mi primera App React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
